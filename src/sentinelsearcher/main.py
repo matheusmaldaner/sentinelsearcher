21b65ed21712c31f03b562fbd48c3bb2f9877999
Matheus Kunzler Maldaner <matheus@sisnema.com.br>
2025-11-03 18:15:37 -0500

    working anthropic search

--- src/sentinelsearcher/main.py
+++ src/sentinelsearcher/main.py
@@ -1,29 +1,35 @@
 
 import anthropic
-import env
+import os
+from dotenv import load_dotenv
 
 def main():
-    print("Welcome to Sentinel Searcher!")
+    # load .env file into environment variables
+    load_dotenv()
 
+    # set the api key from environment variable
+    api_key = os.getenv("ANTHROPIC_API_KEY")
 
-    client = anthropic.Anthropic()
+    # initialize the client with the api key
+    client = anthropic.Anthropic(api_key=api_key)
 
-    response = client.messages.create(
+    # create a message with web search tool enabled
+    message = client.messages.create(
         model="claude-sonnet-4-5",
         max_tokens=1024,
         messages=[
             {
                 "role": "user",
-                "content": "What's the weather in NYC?"
+                "content": "Find the latest 3 news about Matheus Kunzler Maldaner"
             }
         ],
         tools=[{
             "type": "web_search_20250305",
             "name": "web_search",
-            "max_uses": 5
+            #"max_uses": 5
         }]
     )
-    print(response)
+    print(message.content)
 
 
 
